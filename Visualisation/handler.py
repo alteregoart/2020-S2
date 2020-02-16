357fbf46c5b88718d211ba5d18c2985f4d0fc1f6
Arthur Dion <dion@eurecom.fr>
2020-02-16 23:41:39 +0100

    use function and add two new views

--- Visualisation/handler.py
+++ Visualisation/handler.py
@@ -10,6 +10,12 @@ import numpy as np
 
 matplotlib.use('TkAgg')
 
+
+
+
+
+
+
 # This let you download the data and save them on the disk for further use
 # In order to charge the data you have to execute the code in the Vizualisation folder
 
@@ -78,146 +84,181 @@ def get_activity(key,  end_date, begin_date = '2020-01-22', resolution='hour'):
             activities.to_csv('data/rescuetime-' + begin_date + '-to-' + end_date + '.csv')
         except :
             print("Unable to write data on csv")
+    #activities.info()
+    #activities.describe()
+    #activities.tail()
 
+    activities['Productive'] = activities['Productivity']
+    # Format productivity with name
+    activities['Productive'] = activities['Productive'].map({-2: 'très distrayant', 
+                                                            -1: 'distrayant',
+                                                        0: 'Neutre',
+                                                        1: 'Productif',
+                                                        2: 'très productif'})
+
+
+    # print(activities.tail())
+    # Format the date part of the data
+    activities['Date'] = pandas.to_datetime(activities['Date'])
+
+    activities = activities.sort_values(by='Date').reset_index(drop=True)
+
+    activities['DateTime'] = activities['Date']
+    activities['Year'] = activities['Date'].apply(lambda x: x.strftime('%Y'))
+    activities['Month'] = activities['Date'].apply(lambda x: x.strftime('%m'))
+    activities['Hour'] = activities['Date'].apply(lambda x: x.strftime('%H'))
+    activities['Minutes'] = activities['Date'].apply(lambda x: x.strftime('%M'))
+    activities['Date'] = activities['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
     return activities
 
-activities = get_activity(key= KEY, begin_date = '2020-01-22', end_date = '2020-02-05', resolution='hour')
+activities = get_activity(key= KEY, begin_date = '2020-01-22', end_date = '2020-02-13', resolution='hour')
 
-#activities.info()
-#activities.describe()
-#activities.tail()
-
-activities['Productive'] = activities['Productivity']
-# print(activities['Productive'].unique())
-
-activities['Productive'] = activities['Productive'].map({-2: 'très distrayant', 
-                                                        -1: 'distrayant',
-                                                       0: 'Neutre',
-                                                       1: 'Productif',
-                                                       2: 'très productif'})
 
 
 # print(activities.tail())
 
-activities['Date'] = pandas.to_datetime(activities['Date'])
+###Total time per day
+def total_time_per_day(nb_of_day=30):
+    total_computer_time_by_date = activities.groupby(['Date'])['Seconds'].sum().reset_index(name='Seconds')
+    total_computer_time_by_date['Minutes'] = round(total_computer_time_by_date['Seconds'] / 60, 2)
+    total_computer_time_by_date['Hours'] = round(total_computer_time_by_date['Seconds'] / 60 / 60, 2)
+    total_computer_time_by_date.tail()
 
-activities = activities.sort_values(by='Date').reset_index(drop=True)
+    total_computer_time_by_date = total_computer_time_by_date.drop(['Seconds', 'Minutes'], axis=1)
+    total_computer_time_by_date = total_computer_time_by_date.set_index(['Date'])
 
-activities['DateTime'] = activities['Date']
-activities['Year'] = activities['Date'].apply(lambda x: x.strftime('%Y'))
-activities['Month'] = activities['Date'].apply(lambda x: x.strftime('%m'))
-activities['Hour'] = activities['Date'].apply(lambda x: x.strftime('%H'))
-activities['Minutes'] = activities['Date'].apply(lambda x: x.strftime('%M'))
-activities['Date'] = activities['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    chart_title = f"Daily Computer Time in Hours for the last {nb_of_day} Days"
+    # plt.style.use('seaborn-darkgrid')
+    ax = total_computer_time_by_date.tail(nb_of_day).plot.bar(stacked=True, rot=90, figsize=(12,6), colormap='tab20c', legend=False)
+    ax.set_xlabel('')
+    ax.set_ylabel('Hours')
+    ax.set_title(chart_title)
+    plt.show()
 
-# print(activities.tail())
 
-###Total time per Day
 
-total_computer_time_by_date = activities.groupby(['Date'])['Seconds'].sum().reset_index(name='Seconds')
-total_computer_time_by_date['Minutes'] = round(total_computer_time_by_date['Seconds'] / 60, 2)
-total_computer_time_by_date['Hours'] = round(total_computer_time_by_date['Seconds'] / 60 / 60, 2)
-total_computer_time_by_date.tail()
+### Total productivity per day
+def productivity_per_day(nb_of_day=30):
+    total_by_date_productivity = activities.groupby(['Date', 'Productive'])['Seconds'].sum().reset_index(name='Seconds')
+    total_by_date_productivity['Minutes'] = round((total_by_date_productivity['Seconds'] / 60), 2)
+    total_by_date_productivity['Hours'] = round((total_by_date_productivity['Seconds'] / 60 / 60), 2)
+    table = total_by_date_productivity.pivot_table(index='Date', columns='Productive', values='Hours', aggfunc=np.sum)
+    table.to_csv("data/days_productive_time_full.csv")
 
-total_computer_time_by_date = total_computer_time_by_date.drop(['Seconds', 'Minutes'], axis=1)
-total_computer_time_by_date = total_computer_time_by_date.set_index(['Date'])
+    # process and simplify productivity dimensions
+    days_productive_time = table.fillna(value=0).copy()
+    days_productive_time['productive_simple'] = days_productive_time['Productif'] + days_productive_time['très productif']
+    days_productive_time.drop(['Productif', 'très productif'], axis=1, inplace=True)
+    days_productive_time['distracting_simple'] = days_productive_time['distrayant'] + days_productive_time['très distrayant']
+    days_productive_time.drop(['distrayant', 'très distrayant'], axis=1, inplace=True)
+    days_productive_time.columns = ['Neutral', 'Productive', 'Distracting']
 
-chart_title = 'Daily Computer Time in Hours for Last 30 Days'
-# plt.style.use('seaborn-darkgrid')
-ax = total_computer_time_by_date.tail(30).plot.bar(stacked=True, rot=90, figsize=(12,6), colormap='tab20c', legend=False)
-ax.set_xlabel('')
-ax.set_ylabel('Hours')
-ax.set_title(chart_title)
+    chart_title = f'Daily Computer Time Broken Down by Productivity for Last {nb_of_day} Days'
+    # plt.style.use('seaborn-darkgrid')
+    ax = days_productive_time.tail(nb_of_day).plot.bar(stacked=True, rot=90, figsize=(12,6))
+    ax.set_xlabel('')
+    ax.set_ylabel('Hours')
+    ax.set_title(chart_title)
+    plt.show()
+
+
+def heat_map_activity_per_hours():
+    total_time_hours = activities.groupby(['Hour','Date'])['Seconds'].sum().reset_index()
+
+    def heat_map(series, begin_date=None, end_date=None):
+        d1 = datetime.date.fromisoformat(begin_date)
+        d2 = datetime.date.fromisoformat(end_date)
+        delta_time = d2 - d1
+        result = np.zero()
+
+    set_days = set([x[1].Date for x in total_time_hours.iterrows()])
+    n_days = len(set_days)
+    x_labels = sorted(list(set_days))
+    y_labels = [f"{i:02d}" for i in range(24)]
+    heat_map_values = np.zeros((24, n_days))
+
+    for i in range(len(x_labels)):
+        for j in range(len(y_labels)):
+            date = x_labels[i]
+            hour = y_labels[j]
+            temp = total_time_hours[total_time_hours["Hour"] == hour]
+            temp = temp[temp["Date"] == date]["Seconds"]
+            if len(temp) == 0:
+                heat_map_values[j, i] = 0
+            else:
+                assert len(temp) == 1, f"More than 1 entry returned ({len(temp)})"
+                heat_map_values[j, i] = temp.iloc(0)[0]
+
+    # change the representation to start the day at 6pm
+    heat_map_values = np.vstack((heat_map_values[6:], heat_map_values[:6]))
+    heat_map_values = np.round(heat_map_values/60)
+
+    plt.set_cmap("Greys")
+    fig, ax = plt.subplots()
+    im = ax.imshow(heat_map_values)
+    plt.colorbar(im, ax=ax, ticks=range(0, int(np.max(heat_map_values)), int(np.max(heat_map_values / 15))))
+
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(x_labels)))
+    ax.set_yticks(np.arange(len(y_labels)))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(x_labels)
+    ax.set_yticklabels(y_labels[6:] + y_labels[:6])
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    #for i in range(len(y_labels)):
+    #   for j in range(len(x_labels)):
+    #       text = ax.text(j, i, heat_map_values[i, j],
+    #                      ha="center", va="center", color="w")
+
+    ax.set_title("Heatmap: hour representation of day")
+    fig.tight_layout()
+    plt.show()
 #plt.show()
-# plt.savefig("fig.png")
+
+def category_by_time(nb_of_activity=10):
+    categories = activities.pivot_table(index=['Category'], values='Seconds', aggfunc=np.sum).sort_values(by='Seconds', ascending=False)
+    categories['Hours'] = round(categories['Seconds'] / 60 / 60, 1)
 
 
-# total_by_date_productivity
-total_by_date_productivity = activities.groupby(['Date', 'Productive'])['Seconds'].sum().reset_index(name='Seconds')
-total_by_date_productivity['Minutes'] = round((total_by_date_productivity['Seconds'] / 60), 2)
-total_by_date_productivity['Hours'] = round((total_by_date_productivity['Seconds'] / 60 / 60), 2)
-table = total_by_date_productivity.pivot_table(index='Date', columns='Productive', values='Hours', aggfunc=np.sum)
-table.to_csv("data/days_productive_time_full.csv")
+    # Chart Project Time 
+    dataset = categories.head(nb_of_activity)
+    chart_title = 'Category Time Breakdown'
 
-# process and simplify productivity dimensions
-days_productive_time = table.fillna(value=0).copy()
-days_productive_time['productive_simple'] = days_productive_time['Productif'] + days_productive_time['très productif']
-days_productive_time.drop(['Productif', 'très productif'], axis=1, inplace=True)
-days_productive_time['distracting_simple'] = days_productive_time['distrayant'] + days_productive_time['très distrayant']
-days_productive_time.drop(['distrayant', 'très distrayant'], axis=1, inplace=True)
-days_productive_time.columns = ['Neutral', 'Productive', 'Distracting']
+    plt.style.use('seaborn-darkgrid')
+    ax = dataset.plot.bar(y='Hours', figsize=(15, 3), rot=90, colormap='plasma', legend=False)
+    ax.set_ylabel('Hours')
+    ax.set_xlabel('')
 
+    ax.set_title(chart_title)
+    plt.show()
 
-
-chart_title = 'Daily Computer Time Broken Down by Productivity for Last 30 Days'
-# plt.style.use('seaborn-darkgrid')
-ax = days_productive_time.tail(30).plot.bar(stacked=True, rot=90, figsize=(12,6))
-ax.set_xlabel('')
-ax.set_ylabel('Hours')
-ax.set_title(chart_title)
-#plt.show()
-
-#activities.info()
-#print(activities.tail())
-
-total_time_hours = activities.groupby(['Hour','Date'])['Seconds'].sum().reset_index()
-#total_time_hours = 
-#print(total_time_hours)
-#total_time_hours_resample = activities.resample('H','columns' )
-# print(total_time_hours["Seconds"].get_group(("00", "2020-01-25")).sum())
+def app_by_time(nb_of_activity=10):
+    # activity
+    apps = activities.pivot_table(index=['Activity'], values='Seconds', aggfunc=np.sum).sort_values(by='Seconds', ascending=False)
+    apps['Hours'] = round(apps['Seconds'] / 60 / 60, 1)
 
 
-def heat_map(series, begin_date=None, end_date=None):
-    d1 = datetime.date.fromisoformat(begin_date)
-    d2 = datetime.date.fromisoformat(end_date)
-    delta_time = d2 - d1
-    result = np.zero()
+    # Chart Project Time 
+    dataset = apps.head(nb_of_activity)
+    chart_title = 'Category Time Breakdown'
 
-set_days = set([x[1].Date for x in total_time_hours.iterrows()])
-n_days = len(set_days)
-x_labels = sorted(list(set_days))
-y_labels = [f"{i:02d}" for i in range(24)]
-heat_map_values = np.zeros((24, n_days))
+    plt.style.use('seaborn-darkgrid')
+    ax = dataset.plot.bar(y='Hours', figsize=(15, 3), rot=90, colormap='plasma', legend=False)
+    ax.set_ylabel('Hours')
+    ax.set_xlabel('')
 
-for i in range(len(x_labels)):
-	for j in range(len(y_labels)):
-		date = x_labels[i]
-		hour = y_labels[j]
-		temp = total_time_hours[total_time_hours["Hour"] == hour]
-		temp = temp[temp["Date"] == date]["Seconds"]
-		if len(temp) == 0:
-			heat_map_values[j, i] = 0
-		else:
-			assert len(temp) == 1, f"More than 1 entry returned ({len(temp)})"
-			heat_map_values[j, i] = temp.iloc(0)[0]
-
-# change the representation to start the day at 6pm
-heat_map_values = np.vstack((heat_map_values[6:], heat_map_values[:6]))
-heat_map_values = np.round(heat_map_values/60)
-
-plt.set_cmap("Greys")
-fig, ax = plt.subplots()
-im = ax.imshow(heat_map_values)
-plt.colorbar(im, ax=ax, ticks=range(0, int(np.max(heat_map_values)), int(np.max(heat_map_values / 15))))
+    ax.set_title(chart_title)
+    plt.show()
 
 
-# We want to show all ticks...
-ax.set_xticks(np.arange(len(x_labels)))
-ax.set_yticks(np.arange(len(y_labels)))
-# ... and label them with the respective list entries
-ax.set_xticklabels(x_labels)
-ax.set_yticklabels(y_labels[6:] + y_labels[:6])
-
-# Rotate the tick labels and set their alignment.
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-
-# Loop over data dimensions and create text annotations.
-#for i in range(len(y_labels)):
- #   for j in range(len(x_labels)):
- #       text = ax.text(j, i, heat_map_values[i, j],
- #                      ha="center", va="center", color="w")
-
-ax.set_title("Heatmap: hour representation of day")
-fig.tight_layout()
-plt.show()
+#total_time_per_day(30)
+#productivity_per_day(nb_of_day=30)
+#heat_map_activity_per_hours()
+#category_by_time(nb_of_activity=10)
+app_by_time(nb_of_activity= 15)
